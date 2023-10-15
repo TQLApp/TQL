f86a73af51793acbe5a964a36c50cdc5f6416277
Pieter van Ginkel <pvginkel@gmail.com>
2023-10-15 20:39:50 +0200

    Added scripts to create a setup.

---
+++ Build/Support/Build.ps1
@@ -0,0 +1,222 @@
+################################################################################
+# SUPPORT
+################################################################################
+
+Function Get-Script-Directory
+{
+    $Scope = 1
+    
+    while ($True)
+    {
+        $Invoction = (Get-Variable MyInvocation -Scope $Scope).Value
+        
+        if ($Invoction.MyCommand.Path -Ne $Null)
+        {
+            Return Split-Path $Invoction.MyCommand.Path
+        }
+        
+        $Scope = $Scope + 1
+    }
+}
+
+Function Console-Update-Status([string]$Status, [string]$ForegroundColor)
+{
+    $WindowWidth = (Get-Host).UI.RawUI.WindowSize.Width
+    
+    $Position = (Get-Host).UI.RawUI.CursorPosition
+    
+    $Position.X = $WindowWidth - ($Status.Length + 1)
+    $Position.Y = $Position.Y - 1
+    
+    if ($Position.X -ge 0 -and $Position.Y -ge 0)
+    {
+        (Get-Host).UI.RawUI.CursorPosition = $Position
+    }
+    
+    Write-Host $Status -ForegroundColor $ForegroundColor
+}
+
+Function Prepare-Directory([string]$Path)
+{
+    if (Test-Path -Path $Path)
+    {
+        Remove-Item -Recurse -Force $Path
+    }
+
+    [void](New-Item -Type directory $Path)
+}
+
+Function Get-MSBuild
+{
+    Write-Host "Finding MSBuild"
+
+    if (Get-Module VSSetup -ListAvailable)
+    {
+        Import-Module VSSetup
+    }
+    else
+    {
+        Install-Module VSSetup -Scope CurrentUser
+    }
+
+    $vsInstances = Get-VSSetupInstance
+    $vs = @($vsInstances | Select-VSSetupInstance -Require Microsoft.Component.MSBuild)
+    if ($vs)
+    {
+        $MSBuild = Join-Path ($vs[0].InstallationPath) MSBuild\Current\Bin\MSBuild.exe
+    }
+    else
+    {
+        $vs = @($vsInstances | Select-VSSetupInstance -Product Microsoft.VisualStudio.Product.BuildTools)
+        if ($vs)
+        {
+            $MSBuild = Join-Path ($vs[0].InstallationPath) MSBuild\Current\Bin\MSBuild.exe
+        }
+        else
+        {
+            throw "Cannot find MSBuild";
+        }
+    }
+
+    Console-Update-Status "[OK]" -ForegroundColor Green
+
+    return $MSBuild
+}
+
+Function Build-Solution([string]$Solution, [string]$Target = $Null, [string]$Configuration = "Release", [string]$Platform = "Any CPU", [bool]$Skip = $False, [string]$ExtraProperties = $Null, [string]$SolutionDisplay = $Null)
+{
+    if ($Target -eq $Null -or $Target -eq "")
+    {
+        $Target = $Global:DefaultBuildTarget
+    }
+    if ($SolutionDisplay -eq "")
+    {
+        $SolutionDisplay = [System.IO.Path]::GetFileName($Solution)
+    }
+    
+    $Status = "Building " + $SolutionDisplay + " ($Configuration"
+    
+    if ($Target -ne $DefaultBuildTarget)
+    {
+        $Status = $Status + ", " + $Target
+    }
+    
+    $Status = $Status + ")"
+
+    Write-Host $Status
+    
+    if ($Skip)
+    {
+        Console-Update-Status "[SKIPPED]" -ForegroundColor Yellow
+    }
+    else
+    {
+        $Properties = "Platform=`"$Platform`";Configuration=`"$Configuration`";NoWarn=`"1591,1587,1573,0436`""
+        
+        if ($ExtraProperties -ne $Null -and $ExtraProperties -ne "")
+        {
+            $Properties = $Properties + ";" + $ExtraProperties
+        }
+        
+        # Perform the build. Note that warnings CS1591, CS1587 and CS1573 are the warnings for missing
+        # XML documentation. Warning CS0436 is because we need to override a framework
+        # class.
+        
+        $Arguments = "/nr:false /m /verbosity:quiet /nologo `"/t:$Target`" /p:$Properties `"$Solution`""
+        
+        $Process = Start-Process -NoNewWindow -Wait -FilePath $Global:MSBuild -ArgumentList $Arguments -PassThru
+        
+        if ($Process.ExitCode -eq 0)
+        {
+            Console-Update-Status "[OK]" -ForegroundColor Green
+        }
+        else
+        {
+            Console-Update-Status "[FAILED]" -ForegroundColor Red
+            
+            # If we get an error from a build, the chances that we'll be able
+            # to continue are very dim. Because of this, we bail immediately.
+
+            Exit 2
+        }
+    }
+}
+
+Function Copy-Output([string]$From, [string]$Target)
+{
+    Write-Host "Copying $From"
+
+    Copy-Item ($Global:Root + "\" + $From + "\bin\Release\net48") -Destination $Target -Recurse
+
+    Console-Update-Status "[OK]" -ForegroundColor Green
+}
+
+Function Compress-Archive
+{
+    Write-Host "Compressing app"
+    
+    Start-Process `
+        -NoNewWindow `
+        -Wait `
+        -FilePath ($Global:Root + "\Build\Libraries\7z\7za.exe") `
+        -ArgumentList ("a -bd -bso0 -bse0 Archive.7z `"" + $Global:Distrib + "\Bin\*`"") `
+        -WorkingDirectory $Global:Distrib `
+        -PassThru | Out-Null
+    
+    Console-Update-Status "[OK]" -ForegroundColor Green
+}
+
+Function Create-Setup()
+{
+    Write-Host "Creating setup"
+
+    $Config = @"
+;!@Install@!UTF-8!
+Title="Techie's Quick Launcher Setup"
+RunProgram="Tql.Setup.App.exe"
+Progress="no"
+;!@InstallEnd@!
+"@
+
+    $Target = $Global:Distrib + "\Setup.exe"
+    $Header = $Global:Root + "\Build\Support\Header.sfx"
+    $Payload = $Global:Distrib + "\Archive.7z"
+
+    $Output = [System.IO.File]::Create($Target)
+    
+    $Input = [System.IO.File]::OpenRead($Header)
+    $Input.CopyTo($Output)
+    $Input.Dispose()
+    
+    $ConfigBytes = [System.Text.Encoding]::UTF8.GetBytes($Config)
+    $Output.Write($ConfigBytes, 0, $ConfigBytes.Length)
+    
+    $Input = [System.IO.File]::OpenRead($Payload)
+    $Input.CopyTo($Output)
+    $Input.Dispose()
+    
+    $Output.Dispose()
+
+    Console-Update-Status "[OK]" -ForegroundColor Green
+}
+
+################################################################################
+# ENTRY POINT
+################################################################################
+
+$Global:Root = (Get-Item (Get-Script-Directory)).Parent.Parent.FullName
+$Global:DefaultBuildTarget = "Build"
+$Global:Distrib = $Global:Root + "\Build\Distrib"
+$Global:MSBuild = Get-MSBuild
+
+<#
+Prepare-Directory -Path $Global:Distrib
+
+Build-Solution -Solution ($Global:Root + "\Tql.sln")
+
+Copy-Output -From "Tql.Setup.App" -Target ($Global:Distrib + "\Bin")
+Copy-Output -From "Tql.App" -Target ($Global:Distrib + "\Bin\Assets")
+#>
+Compress-Archive
+
+Create-Setup
