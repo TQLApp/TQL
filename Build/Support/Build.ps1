68be2466521b0b472b863e59e1701d3b08477dd6
Pieter van Ginkel <pvginkel@gmail.com>
2023-10-15 20:40:21 +0200

    Minor fix.

--- Build/Support/Build.ps1
+++ Build/Support/Build.ps1
@@ -209,14 +209,13 @@ $Global:DefaultBuildTarget = "Build"
 $Global:Distrib = $Global:Root + "\Build\Distrib"
 $Global:MSBuild = Get-MSBuild
 
-<#
 Prepare-Directory -Path $Global:Distrib
 
 Build-Solution -Solution ($Global:Root + "\Tql.sln")
 
 Copy-Output -From "Tql.Setup.App" -Target ($Global:Distrib + "\Bin")
 Copy-Output -From "Tql.App" -Target ($Global:Distrib + "\Bin\Assets")
-#>
+
 Compress-Archive
 
 Create-Setup
