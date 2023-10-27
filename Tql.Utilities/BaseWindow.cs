c37aba4ecea57e5935c37b62d1583482ce634869
Pieter van Ginkel <pvginkel@gmail.com>
2023-10-27 05:38:49 +0200

    Enabled immerse dark mode.

--- Tql.Utilities/BaseWindow.cs
+++ Tql.Utilities/BaseWindow.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Media;
+﻿using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Windows.Media;
 
 namespace Tql.Utilities;
 
@@ -12,4 +14,24 @@ public class BaseWindow : Window
         RenderOptions.SetClearTypeHint(this, ClearTypeHint.Enabled);
         Style = (Style)FindResource("CustomWindowStyle");
     }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+
+        var interop = new WindowInteropHelper(this);
+
+        var value = 1;
+        DwmSetWindowAttribute(interop.Handle, DWMWA_NCRENDERING_POLICY, ref value, sizeof(int));
+    }
+
+    [DllImport("dwmapi.dll")]
+    internal static extern int DwmSetWindowAttribute(
+        IntPtr hwnd,
+        uint attr,
+        ref int attrValue,
+        int attrSize
+    );
+
+    private const uint DWMWA_NCRENDERING_POLICY = 20;
 }
