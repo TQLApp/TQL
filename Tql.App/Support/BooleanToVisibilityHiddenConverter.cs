c581f550745dce0d5aa0afdadceae7af8883ff0e
Pieter van Ginkel <pvginkel@gmail.com>
2023-10-29 17:04:13 +0100

    Minor UI enhancements in the plugin settings page.

---
+++ Tql.App/Support/BooleanToVisibilityHiddenConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Tql.App.Support;
+
+internal class BooleanToVisibilityHiddenConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not bool boolValue)
+            boolValue = false;
+
+        return boolValue ? Visibility.Visible : Visibility.Hidden;
+    }
+
+    public object ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture
+    )
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ Tql.App/Support/InvertedBooleanToVisibilityHiddenConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Tql.App.Support;
+
+internal class InvertedBooleanToVisibilityHiddenConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not bool boolValue)
+            boolValue = false;
+
+        return boolValue ? Visibility.Hidden : Visibility.Visible;
+    }
+
+    public object ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture
+    )
+    {
+        throw new NotSupportedException();
+    }
+}
