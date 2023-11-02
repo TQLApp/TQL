42942e022d1053c5a213d73ec28e1da668cd20aa
Pieter van Ginkel <pvginkel@gmail.com>
2023-11-02 07:45:37 +0100

    Added script to prettify XAML.

---
+++ runprettier.bat
@@ -0,0 +1,19 @@
+@echo off
+
+pushd "%~dp0"
+
+call npm install --save-dev prettier @prettier/plugin-xml
+
+call npx prettier ^
+  --plugin=@prettier/plugin-xml ^
+  --write "**/*.xaml" ^
+  --end-of-line crlf ^
+  --bracket-same-line true ^
+  --single-attribute-per-line true ^
+  --xml-quote-attributes double ^
+  --xml-self-closing-space true ^
+  --xml-whitespace-sensitivity ignore
+
+pause
+
+popd
