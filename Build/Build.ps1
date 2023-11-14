e3fa4d174d7047862c4c29461b1e1f0fe6d53bc7
Pieter van Ginkel <pvginkel@gmail.com>
2023-11-14 12:10:37 +0100

    Added build script to be able to do local release builds for testing.

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -19,97 +19,136 @@ Function Get-Script-Directory
     }
 }
 
+Function Console-Update-Status([string]$Status, [string]$ForegroundColor)
+{
+    $WindowWidth = (Get-Host).UI.RawUI.WindowSize.Width
+    
+    $Position = (Get-Host).UI.RawUI.CursorPosition
+    
+    $Position.X = $WindowWidth - ($Status.Length + 1)
+    $Position.Y = $Position.Y - 1
+    
+    if ($Position.X -ge 0 -and $Position.Y -ge 0)
+    {
+        (Get-Host).UI.RawUI.CursorPosition = $Position
+    }
+    
+    Write-Host $Status -ForegroundColor $ForegroundColor
+}
+
+Function Prepare-Directory([string]$Path)
+{
+    if (Test-Path -Path $Path)
+    {
+        Remove-Item -Recurse -Force $Path
+    }
+
+    [void](New-Item -Type directory $Path)
+}
+
+Function Get-MSBuild
+{
+    Write-Host "Finding MSBuild"
+
+    if (Get-Module VSSetup -ListAvailable)
+    {
+        Import-Module VSSetup
+    }
+    else
+    {
+        Install-Module VSSetup -Scope CurrentUser
+    }
+
+    $vsInstances = Get-VSSetupInstance
+    $vs = @($vsInstances | Select-VSSetupInstance -Require Microsoft.Component.MSBuild)
+    if ($vs)
+    {
+        $MSBuild = Join-Path ($vs[0].InstallationPath) MSBuild\Current\Bin\MSBuild.exe
+    }
+    else
+    {
+        $vs = @($vsInstances | Select-VSSetupInstance -Product Microsoft.VisualStudio.Product.BuildTools)
+        if ($vs)
+        {
+            $MSBuild = Join-Path ($vs[0].InstallationPath) MSBuild\Current\Bin\MSBuild.exe
+        }
+        else
+        {
+            throw "Cannot find MSBuild";
+        }
+    }
+
+    Console-Update-Status "[OK]" -ForegroundColor Green
+
+    return $MSBuild
+}
+
+Function Build-Solution([string]$Solution, [string]$Target = $Null, [string]$Configuration = "Release", [string]$Platform = "Any CPU", [bool]$Skip = $False, [string]$ExtraProperties = $Null, [string]$SolutionDisplay = $Null)
+{
+    if ($Target -eq $Null -or $Target -eq "")
+    {
+        $Target = $Global:DefaultBuildTarget
+    }
+    if ($SolutionDisplay -eq "")
+    {
+        $SolutionDisplay = [System.IO.Path]::GetFileName($Solution)
+    }
+    
+    $Status = "Building " + $SolutionDisplay + " ($Configuration"
+    
+    if ($Target -ne $DefaultBuildTarget)
+    {
+        $Status = $Status + ", " + $Target
+    }
+    
+    $Status = $Status + ")"
+
+    Write-Host $Status
+    
+    if ($Skip)
+    {
+        Console-Update-Status "[SKIPPED]" -ForegroundColor Yellow
+    }
+    else
+    {
+        $Properties = "Platform=`"$Platform`";Configuration=`"$Configuration`";NoWarn=`"1591,1587,1573,0436`""
+        
+        if ($ExtraProperties -ne $Null -and $ExtraProperties -ne "")
+        {
+            $Properties = $Properties + ";" + $ExtraProperties
+        }
+        
+        # Perform the build. Note that warnings CS1591, CS1587 and CS1573 are the warnings for missing
+        # XML documentation. Warning CS0436 is because we need to override a framework
+        # class.
+        
+        $Arguments = "/nr:false /m /verbosity:quiet /nologo `"/t:$Target`" /p:$Properties `"$Solution`""
+        
+        $Process = Start-Process -NoNewWindow -Wait -FilePath $Global:MSBuild -ArgumentList $Arguments -PassThru
+        
+        if ($Process.ExitCode -eq 0)
+        {
+            Console-Update-Status "[OK]" -ForegroundColor Green
+        }
+        else
+        {
+            Console-Update-Status "[FAILED]" -ForegroundColor Red
+            
+            # If we get an error from a build, the chances that we'll be able
+            # to continue are very dim. Because of this, we bail immediately.
+
+            Exit 2
+        }
+    }
+}
+
 Function Copy-Output([string]$From, [string]$Target)
 {
     Write-Host "Copying $From"
 
-    Copy-Item "$Global:Root\$From\bin\Release\net48" -Destination $Target -Recurse
-    
-    Get-ChildItem $Target
-}
+    Copy-Item ($Global:Root + "\" + $From + "\bin\Release\net48") -Destination $Target -Recurse
 
-Function Ensure-WiX
-{
-    $Url = "https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip"
-    $Target = "$env:TEMP\wix3112rtm"
-
-    if (-not (Test-Path -Path $Target))
-    {
-        Write-Host "Downloading WiX"
-
-        Invoke-WebRequest -Uri $Url -OutFile "$Target.zip"
-        Expand-Archive -Path "$Target.zip" -DestinationPath $Target
-    }
-
-    $Global:WiXRoot = $Target
-}
-
-Function Run-WiX-Command([string]$File, [string]$ArgumentList, [string]$WorkingDirectory)
-{
-    Write-Host "Executing $File $ArgumentList"
-
-    $Process = Start-Process `
-        -NoNewWindow `
-        -Wait `
-        -FilePath "$Global:WiXRoot\$File" `
-        -ArgumentList $ArgumentList `
-        -WorkingDirectory $WorkingDirectory `
-        -PassThru
-    
-    if ($Process.ExitCode -ne 0)
-    {
-        Write-Host "Command failed with exit code $($Process.ExitCode)"
-        exit $Process.ExitCode
-    }
-}
-
-Function Get-App-Version
-{
-    $Content = [System.IO.File]::ReadAllText("$Global:Root\Build\Version.wxi")
-
-    if (-not ($Content -match "Version=`"(.*?)`""))
-    {
-        Write-Host "Could not parse Version.wxi"
-        exit 1
-    }
-
-    return $Matches[1]
-}
-
-Function Create-MSI
-{
-    Write-Host "Collecting source files"
-
-    Run-WiX-Command `
-        -File "heat.exe" `
-        -ArgumentList "dir . -nologo -v -dr MyBinFolder -srd -cg MyAppComponents -gg -sfrag -scom -sreg -out ..\AppFiles.wxs" `
-        -WorkingDirectory "$Global:Distrib\SourceDir"
-
-    Get-Content "$Global:Distrib\AppFiles.wxs"
-
-    Write-Host "Compiling Tql.wxs"
-
-    Run-WiX-Command `
-        -File "candle.exe" `
-        -ArgumentList "-nologo -v $Global:Root\Build\Tql.wxs" `
-        -WorkingDirectory $Global:Distrib
-
-    Write-Host "Compiling AppFiles.wxs"
-
-    Run-WiX-Command `
-        -File "candle.exe" `
-        -ArgumentList "-nologo -v $Global:Root\Build\Distrib\AppFiles.wxs" `
-        -WorkingDirectory $Global:Distrib
-
-    $Version = Get-App-Version
-    $Output = "Tql-$Version.msi"
-
-    Write-Host "Linking $Output"
-
-    Run-WiX-Command `
-        -File "light.exe" `
-        -ArgumentList "-nologo -v Tql.wixobj AppFiles.wixobj -ext WixUIExtension -o $Output -sice:ICE91 -sice:ICE64 -sice:ICE38" `
-        -WorkingDirectory $Global:Distrib
+    Console-Update-Status "[OK]" -ForegroundColor Green
 }
 
 ################################################################################
@@ -118,11 +157,11 @@ Function Create-MSI
 
 $Global:Root = (Get-Item (Get-Script-Directory)).Parent.FullName
 $Global:DefaultBuildTarget = "Build"
-$Global:Distrib = "$Global:Root\Build\Distrib"
+$Global:Distrib = $Global:Root + "\Build\Distrib"
+$Global:MSBuild = Get-MSBuild
 
-Get-ChildItem -Path $Global:Distrib -Recurse | Remove-Item -Force -Recurse
-Copy-Output -From "Tql.App" -Target "$Global:Distrib\SourceDir"
+Prepare-Directory -Path $Global:Distrib
 
-Ensure-WiX
+Build-Solution -Solution ($Global:Root + "\Tql.sln")
 
-Create-MSI
+Copy-Output -From "Tql.App" -Target $Global:Distrib
