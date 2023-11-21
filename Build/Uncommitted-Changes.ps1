58f88764e746b0d5b341662f311394c06d4cd294
Pieter van Ginkel <pvginkel@gmail.com>
2023-11-21 09:50:50 +0100

    Change uncommitted changes message into an error.

--- Build/Uncommitted-Changes.ps1
+++ Build/Uncommitted-Changes.ps1
@@ -4,6 +4,6 @@ $Changes = git status --porcelain
 
 if ("$Changes".Trim() -ne "")
 {
-    Write-Host "There are uncommitted changes. Run '$tool' and update your PR with any changes."
+    Write-Host "::error::There are uncommitted changes. Run '$tool' and update your PR with any changes."
     exit 1
 }
