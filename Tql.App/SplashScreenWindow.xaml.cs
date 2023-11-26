f52ab95e88d2336f3aca07a38daef69272719ec0
Pieter van Ginkel <pvginkel@gmail.com>
2023-11-26 22:50:04 +0100

    Added a splash window.

---
+++ Tql.App/SplashScreenWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.IO;
+using System.Resources;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+using Tql.Utilities;
+
+namespace Tql.App;
+
+internal partial class SplashScreenWindow
+{
+    private readonly List<ImageResource> _images;
+    private readonly DispatcherTimer _timer;
+    private readonly Random _random = new();
+
+    public SplashScreenWindow()
+    {
+        InitializeComponent();
+
+        _images = LoadImages().ToList();
+
+        _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+        _timer.Tick += _timer_Tick;
+        _timer.Start();
+
+        _image1.Source = _image2.Source = GetRandomImage();
+    }
+
+    private void _timer_Tick(object? sender, EventArgs e)
+    {
+        _image1.Source = _image2.Source;
+
+        Canvas.SetLeft(_image1, 0);
+        Canvas.SetLeft(_image2, _image1.Width);
+
+        _image2.Source = GetRandomImage();
+
+        var animation1 = new DoubleAnimation
+        {
+            From = 0,
+            To = -(_image1.Width + 5),
+            Duration = TimeSpan.FromSeconds(0.4)
+        };
+
+        _image1.BeginAnimation(Canvas.LeftProperty, animation1);
+
+        var animation2 = new DoubleAnimation
+        {
+            From = _image1.Width + 5,
+            To = 0,
+            Duration = TimeSpan.FromSeconds(0.4)
+        };
+
+        _image2.BeginAnimation(Canvas.LeftProperty, animation2);
+    }
+
+    private DrawingImage GetRandomImage()
+    {
+        var imageResource = _images[_random.Next(0, _images.Count)];
+
+        imageResource.LoadedImage ??= ImageFactory.CreateSvgImage(imageResource.Stream);
+
+        return imageResource.LoadedImage;
+    }
+
+    private IEnumerable<ImageResource> LoadImages()
+    {
+        var resourceManager = new ResourceManager(
+            $"{GetType().Assembly.GetName().Name}.g",
+            GetType().Assembly
+        );
+        foreach (
+            var resource in resourceManager
+                .GetResourceSet(CultureInfo.CurrentUICulture, true, true)!
+                .Cast<DictionaryEntry>()
+        )
+        {
+            var resourceName = (string)resource.Key;
+            var resourceStream = (Stream)resource.Value!;
+
+            if (resourceName.Contains("/universe%20icons/", StringComparison.OrdinalIgnoreCase))
+                yield return new ImageResource(resourceStream);
+        }
+    }
+
+    private record ImageResource(Stream Stream)
+    {
+        public DrawingImage? LoadedImage { get; set; }
+    }
+
+    public void SetProgress(double progress)
+    {
+        _progressBar.Value = progress;
+    }
+}
