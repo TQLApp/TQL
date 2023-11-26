9dc418b40504deafe78447c3eabf5d6262a1ec48
Pieter van Ginkel <pvginkel@gmail.com>
2023-11-26 07:34:28 +0100

    Switched to a stable hash code.
    Closes #84.

---
+++ Tql.App/Support/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Tql.App.Support;
+
+internal static class StringExtensions
+{
+    public static int GetStableHashCode(this string self)
+    {
+        var hashCode = 0;
+
+        foreach (var c in self)
+        {
+            hashCode = (hashCode * 397) ^ (int)c;
+        }
+
+        return hashCode;
+    }
+}
