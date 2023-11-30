7ffb2665a1f57babd6c35a11ced255e788ef9bf1
Pieter van Ginkel <pvginkel@gmail.com>
2023-11-30 14:30:04 +0100

    Reworked the prettier script.

--- Build/Run-Prettier.ps1
+++ Build/Run-Prettier.ps1
@@ -2,13 +2,30 @@
 # SUPPORT
 ################################################################################
 
+Function Get-Script-Directory
+{
+    $Scope = 1
+    
+    while ($True)
+    {
+        $Invoction = (Get-Variable MyInvocation -Scope $Scope).Value
+        
+        if ($Invoction.MyCommand.Path -Ne $Null)
+        {
+            Return Split-Path $Invoction.MyCommand.Path
+        }
+        
+        $Scope = $Scope + 1
+    }
+}
+
 Function Split-Playbook
 {
-    foreach ($Child in Get-ChildItem "Tql.App\QuickStart\Playbook*.md")
+    foreach ($Child in Get-ChildItem "$Global:Root\Tql.App\QuickStart\Playbook*.md")
     {
         $Index = 0
         $Content = Get-Content $Child.FullName -Encoding UTF8
-        $Part = @()
+        $Part = New-Object System.Collections.Generic.List[string]
         $First = $true
 
         foreach ($Line in $Content)
@@ -20,7 +37,7 @@ Function Split-Playbook
                     Write-Playbook-Part -Name $Child.Name -Index $Index -Part $Part
 
                     $First = $false
-                    $Part = @()
+                    $Part = New-Object System.Collections.Generic.List[string]
                     $Index += 1
                 }
                 else
@@ -29,15 +46,20 @@ Function Split-Playbook
                 }
             }
 
-            $Part += $Line
+            $Part.Add($Line)
         }
 
         Write-Playbook-Part -Name $Child.Name -Index $Index -Part $Part
     }
 }
 
-Function Write-Playbook-Part([string]$Name, [int]$Index, [array]$Part)
+Function Write-Playbook-Part([string]$Name, [int]$Index, $Part)
 {
+    while ($Part.Count -gt 0 -and $Part[$Part.Count - 1] -eq "")
+    {
+        $Part.RemoveAt($Part.Count - 1)
+    }
+
     if ($Part.Count -gt 0)
     {
         Set-Content (Get-Playbook-Part-FileName -Name $Name -Index $Index) $Part -Encoding UTF8
@@ -46,7 +68,7 @@ Function Write-Playbook-Part([string]$Name, [int]$Index, [array]$Part)
 
 Function Merge-Playbook
 {
-    foreach ($Child in Get-ChildItem "Tql.App\QuickStart\Playbook*.md")
+    foreach ($Child in Get-ChildItem "$Global:Root\Tql.App\QuickStart\Playbook*.md")
     {
         $Content = @()
 
@@ -87,22 +109,33 @@ Function Get-Playbook-Part-FileName([string]$Name, [int]$Index)
 {
     $Name = $Name.Substring(0, $Name.Length - 3)
 
-    return "Tql.App\QuickStart\Split$Name-$Index.md"
+    return "$Global:Root\Tql.App\QuickStart\Split$Name-$Index.md"
 }
 
 ################################################################################
 # ENTRY POINT
 ################################################################################
 
-npm install --save-dev prettier @prettier/plugin-xml
+$Global:Root = (Get-Item (Get-Script-Directory)).Parent.FullName
 
-rm "Tql.App\QuickStart\SplitPlaybook*.md"
+$IsVSCode = ($env:TERM_PROGRAM -eq "vscode")
+
+if ($IsVSCode)
+{
+    Write-Host "Skipping NPM install because you're running this from VSCode. Start the /runprettier.bat script from outside of VSCode to run NPM install."
+}
+else
+{
+    npm install --save-dev prettier @prettier/plugin-xml
+}
+
+rm "$Global:Root\Tql.App\QuickStart\SplitPlaybook*.md"
 
 Split-Playbook
 
 npx prettier `
   --plugin=@prettier/plugin-xml `
-  --write "**/*.xaml" "**/*.md" `
+  --write "$Global:Root\**\*.xaml" "$Global:Root\**\*.md" `
   --end-of-line crlf `
   --bracket-same-line true `
   --single-attribute-per-line true `
@@ -113,4 +146,4 @@ npx prettier `
 
 Merge-Playbook
 
-rm "Tql.App\QuickStart\SplitPlaybook*.md"
+rm "$Global:Root\Tql.App\QuickStart\SplitPlaybook*.md"
