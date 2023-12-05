660e875d013c056c17ce93f8346c09ba4070158f
Pieter van Ginkel <pvginkel@gmail.com>
2023-12-05 21:02:28 +0100

    Cache repositories.

--- src/Tql.Plugins.GitHub/Data/GitHubData.cs
+++ src/Tql.Plugins.GitHub/Data/GitHubData.cs
@@ -5,5 +5,13 @@ internal record GitHubData(ImmutableArray<GitHubConnectionData> Connections);
 internal record GitHubConnectionData(
     Guid Id,
     string UserName,
-    ImmutableArray<string> Organizations
+    ImmutableArray<string> Organizations,
+    ImmutableArray<GitHubRepository> Repositories
+);
+
+internal record GitHubRepository(
+    string Owner,
+    string Name,
+    string HtmlUrl,
+    DateTimeOffset UpdatedAt
 );
