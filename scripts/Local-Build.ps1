4dccc3f35a7b28124b286a25998eaac4024c701b
Pieter van Ginkel <pvginkel@gmail.com>
2023-12-11 07:14:49 +0100

    Copy packages on a local build.

--- scripts/Local-Build.ps1
+++ scripts/Local-Build.ps1
@@ -13,6 +13,15 @@ Function Copy-Output([string]$From, [string]$Target)
     Copy-Item "$Global:Src\$From\bin\Release\net8.0-windows\*" -Destination $Target -Recurse
 }
 
+Function Copy-Packages([string]$Target)
+{
+    Write-Host "Copying NuGet packages"
+
+    [void](New-Item -Type directory $Target)
+
+    Get-ChildItem -Path $Global:Src -Recurse -Filter "*.nupkg" | Copy-Item -Destination $Target -Force
+}
+
 ################################################################################
 # ENTRY POINT
 ################################################################################
@@ -30,3 +39,5 @@ dotnet build -c Release -property:Version=999.0
 
 Copy-Output -From "Tql.App" -Target "$Global:Distrib\App"
 Copy-Output -From "Tql.DebugApp" -Target "$Global:Distrib\DebugApp"
+
+Copy-Packages -Target "$Global:Distrib\Packages"
